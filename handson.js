fc3f78f4d85381849c2b0c8a75101213db638219
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-02-05 11:53:40 +0800

    Membuat Array of Objects

---
+++ handson.js
@@ -0,0 +1,43 @@
+let Nakama = [
+    {
+        nama:"zoro",
+        nik:"322",
+        jenis_kelamin:"laki-laki",
+        gol_dar:"O",
+        asal:"jakarta",
+        domisili:"Jayapura",
+        hobi:["Tidur","Gim","Minum Jus"]
+    },
+    {
+        nama:"Luffi",
+        nik:"246",
+        jenis_kelamin:"laki-laki",
+        gol_dar:"B",
+        asal:"Bekasi",
+        domisili:"Makasar",
+        hobi:["Makan","Memancing","Joging"]
+    },
+    {
+        nama:"Sanji",
+        nik:"311",
+        jenis_kelamin:"laki-laki",
+        gol_dar:"A",
+        asal:"Depok",
+        domisili:"Yogyakarta",
+        hobi:["Memasak","Baca Buku","Nyemil"]
+    },
+    {
+        nama:"Nami",
+        nik:"571",
+        jenis_kelamin:"Perempuan",
+        gol_dar:"A",
+        asal:"Bandung",
+        domisili:"Jakarta",
+        hobi:["Belanja","Perawatan","Tidur"]
+    },
+];
+for (let i = 0; i < Nakama.length; i++) {
+    
+      console.log(Nakama[i].nama);
+   
+  }
